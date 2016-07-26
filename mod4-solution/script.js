0a5f7e055dc22b43bb59329bc145b075e5cf0bc3
Dzmitry Chaulytka <dchavlytko@gmail.com>
2016-07-26 09:27:50 +0300

    upd

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -49,9 +49,8 @@ WARNING!!! WARNING!!!
 	  // names that start with either upper case or lower case 'J'/'j', call
 	  // string object's 'toLowerCase' method on the result so we can compare
 	  // to lower case character 'j' afterwards.
-	  // Look up these methods on Mozilla Developer Network web site if
-		// needed.
-	   var firstLetter = name.charAt(0).toLowerCase();
+	  // Look up these methods on Mozilla Developer Network web site if needed.
+	  var firstLetter = name.charAt(0).toLowerCase();
 
 	  // STEP 12:
 	  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -60,11 +59,12 @@ WARNING!!! WARNING!!!
 	  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the
 		// current
 	  // name in the loop.
-	 if (firstLetter === 'j') {
-		 byeSpeaker.speak(name);
-	 } else {
-		 helloSpeaker.speak(name);
-	 }
+	 
+	  if (firstLetter === 'j') {
+	  	byeSpeaker.speak(name);
+	  } else {
+	    helloSpeaker.speak(name);
+	  }
 	}
 })();
 
